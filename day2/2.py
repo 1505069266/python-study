3840bc624e8eb2cfd00c46ce965e7e051a1c3ba1
zhuxiaole <1505069266@qq.com>
2019-09-14 21:20:13 +0800

    9.14

---
+++ day2/2.py
@@ -0,0 +1,18 @@
+
+# 将一个对象转化成整数类型  int(对象)
+age = "20"
+
+print(type(age))
+
+if int(age) > 18:
+    print("已经成年  可以去网吧")
+    print("已经成年  可以去嫖娼")
+else:
+    print("未成年人禁止上网")
+
+
+name = "朱晓乐"
+zhu_age = 23
+zhu_height = "170cm"
+
+print("我叫%s,我今年%d岁了,身高%s"%(name,zhu_age,zhu_height))
